f1818790a177e9f438edfe12a35fb5477ea237a5
Tschaika <tschaika@protonmail.com>
2023-05-07 11:38:26 +0200

    added sounds, improved layout

--- src/app/components/tournament/overview/overview.component.ts
+++ src/app/components/tournament/overview/overview.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, Input, OnInit, ViewChild } from '@angular/core';
 import { BehaviorSubject, Observable, of } from 'rxjs';
 import { debounceTime, map } from 'rxjs/operators';
 import { blindLevelsNoAnte } from 'src/app/shared/data/blind-levels.const';
@@ -6,131 +6,137 @@ import { BlindLevel } from 'src/app/shared/models/blind-level.interface';
 import { Pause } from 'src/app/shared/models/pause.interface';
 
 @Component({
-  selector: 'app-overview',
-  templateUrl: './overview.component.html',
-  styleUrls: ['./overview.component.scss'],
+    selector: 'app-overview',
+    templateUrl: './overview.component.html',
+    styleUrls: ['./overview.component.scss'],
 })
 export class OverviewComponent implements OnInit {
-  @Input() levels: (BlindLevel | Pause)[] = blindLevelsNoAnte;
-  currentLevelIndex = 0;
-  currentLevelTimeLeft: number = 0;
-  currentTimeLeftPercentage: number = 100;
+    @Input() levels: (BlindLevel | Pause)[] = blindLevelsNoAnte;
+    currentLevelIndex = 0;
+    currentLevelTimeLeft: number = 0;
+    currentTimeLeftPercentage: number = 100;
 
-  counter$!: Observable<number>;
-  resize$ = new BehaviorSubject<{ width: number; height: number }>({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
-
-
-  blindDuration: number = 0;
-  blindDurationFixed: number = 0;
-  radius$: Observable<number> = of(200);
-  radius = window.innerHeight * 0.6 * 0.4;
-  innerStroke$: Observable<number> = of(10);
-  innerStroke = window.innerHeight * 0.01;
-  outerStroke$: Observable<number> = of(32);
-  outerStroke = window.innerHeight * 0.05;
-
-  showTimer = true;
-  running = false;
-  finished = false;
-
-  @HostListener('window:resize', ['$event.target'])
-  onResize(target: any): void {
-    this.resize$.next({
-      width: target.innerWidth,
-      height: target.innerHeight,
+    resize$ = new BehaviorSubject<{ width: number; height: number }>({
+        width: window.innerWidth,
+        height: window.innerHeight,
     });
-  }
 
-  @HostListener('window:keyup', ['$event'])
-  onKeydownHandler(event: KeyboardEvent) {
-    if (event.keyCode === 32) {
-      if (this.running) {
-        this.pause();
-      } else {
-        this.start();
-      }
+    blindDuration: number = 0;
+    blindDurationFixed: number = 0;
+    radius$: Observable<number> = of(200);
+    radius = window.innerHeight * 0.6 * 0.4;
+    innerStroke$: Observable<number> = of(10);
+    innerStroke = window.innerHeight * 0.01;
+    outerStroke$: Observable<number> = of(32);
+    outerStroke = window.innerHeight * 0.05;
+
+    showTimer = true;
+    running = false;
+    finished = false;
+
+    @ViewChild('warning') warning!: ElementRef;
+    @ViewChild('next') next!: ElementRef;
+
+    @HostListener('window:resize', ['$event.target'])
+    onResize(target: any): void {
+        this.resize$.next({
+            width: target.innerWidth,
+            height: target.innerHeight,
+        });
     }
-  }
 
-  ngOnInit(): void {
-    this.levels = this.levels.map((level) => ({
-      ...level,
-    }));
-
-    this.currentLevelTimeLeft = this.levels[this.currentLevelIndex].durationMinutes * 60;
-    this.blindDuration =
-      this.levels[this.currentLevelIndex].durationMinutes * 60;
-
-    this.blindDurationFixed =
-      this.levels[this.currentLevelIndex].durationMinutes * 60;
-
-    this.radius$ = this.resize$.pipe(
-      debounceTime(100),
-      map((size) => size.height * 0.6 * 0.4)
-    );
-
-    this.innerStroke$ = this.resize$.pipe(
-      debounceTime(100),
-      map((size) => size.height * 0.01)
-    );
-
-    this.outerStroke$ = this.resize$.pipe(
-      debounceTime(100),
-      map((size) => size.height * 0.05)
-    );
-  }
-
-  private setupWakeLock(): void {
-    const anyNav: any = navigator
-    if ('wakeLock' in navigator) {
-      anyNav['wakeLock'].request('screen')
+    @HostListener('window:keyup', ['$event'])
+    onKeydownHandler(event: KeyboardEvent) {
+        if (event.keyCode === 32) {
+            if (this.running) {
+                this.pause();
+            } else {
+                this.start();
+            }
+        }
     }
-  }
 
-  onValue(event: number): void {
-    this.currentLevelTimeLeft = event;
+    ngOnInit(): void {
+        this.levels = this.levels.map((level) => ({
+            ...level,
+        }));
 
-    console.log(this.currentLevelTimeLeft);
-    console.log(this.blindDurationFixed);
+        this.currentLevelTimeLeft = this.levels[this.currentLevelIndex].durationMinutes * 60;
+        this.blindDuration =
+            this.levels[this.currentLevelIndex].durationMinutes * 60;
 
-    this.currentTimeLeftPercentage =
-      (this.currentLevelTimeLeft / this.blindDurationFixed) * 100;
+        this.blindDurationFixed =
+            this.levels[this.currentLevelIndex].durationMinutes * 60;
 
-    if (this.currentLevelTimeLeft === 0) {
-      this.showTimer = false;
-      this.currentLevelIndex++;
+        this.radius$ = this.resize$.pipe(
+            debounceTime(100),
+            map((size) => size.height * 0.6 * 0.4)
+        );
 
-      if (this.currentLevelIndex === this.levels.length) {
-        this.finished = true;
-      } else {
-        setTimeout(() => {
-          this.currentLevelTimeLeft =
-            (this.levels[this.currentLevelTimeLeft].durationMinutes * 60 )+ 1;
-          this.blindDuration = this.currentLevelTimeLeft;
+        this.innerStroke$ = this.resize$.pipe(
+            debounceTime(100),
+            map((size) => size.height * 0.01)
+        );
 
-          this.blindDurationFixed = this.currentLevelTimeLeft;
-          this.currentTimeLeftPercentage = 100;
-          this.showTimer = true;
-
-        }, 1000);
-
-      }
+        this.outerStroke$ = this.resize$.pipe(
+            debounceTime(100),
+            map((size) => size.height * 0.05)
+        );
     }
-  }
 
-  start(): void {
-    if (!this.running) {
-      this.running = true;
+    private setupWakeLock(): void {
+        const anyNav: any = navigator;
+        if ('wakeLock' in navigator) {
+            anyNav['wakeLock'].request('screen');
+        }
     }
-  }
 
-  pause(): void {
-    if (this.running) {
-      this.running = false;
-      this.blindDuration = this.currentLevelTimeLeft + 1;
+    onValue(event: number): void {
+        this.currentLevelTimeLeft = event;
+
+        if (this.currentLevelTimeLeft === 60 && this.warning) {
+            this.warning.nativeElement.play();
+        }
+
+        if (this.currentLevelTimeLeft === 0 && this.next) {
+            this.next.nativeElement.play();
+        }
+
+        this.currentTimeLeftPercentage =
+            (this.currentLevelTimeLeft / this.blindDurationFixed) * 100;
+
+        if (this.currentLevelTimeLeft === 0) {
+            this.showTimer = false;
+            this.currentLevelIndex++;
+
+            if (this.currentLevelIndex === this.levels.length) {
+                this.finished = true;
+            } else {
+                setTimeout(() => {
+                    this.currentLevelTimeLeft =
+                        (this.levels[this.currentLevelTimeLeft].durationMinutes * 60) + 1;
+                    this.blindDuration = this.currentLevelTimeLeft;
+
+                    this.blindDurationFixed = this.currentLevelTimeLeft;
+                    this.currentTimeLeftPercentage = 100;
+                    this.showTimer = true;
+
+                }, 1000);
+
+            }
+        }
     }
-  }
+
+    start(): void {
+        if (!this.running) {
+            this.running = true;
+        }
+    }
+
+    pause(): void {
+        if (this.running) {
+            this.running = false;
+            this.blindDuration = this.currentLevelTimeLeft + 1;
+        }
+    }
 }
